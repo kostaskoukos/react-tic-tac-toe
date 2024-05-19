4ff9746d6ece620577c2b81363e3fe18f10622b9
kostaskoukos <kkonst2014@gmail.com>
2024-05-19 15:59:23 +0300

    setup cell and board

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+function Cell({ val }: { val?: "x" | "o" }) {
+  return (
+    <button className="bg-gray-200 text-4xl min-w-10 min-h-10">{val}</button>
+  );
+}
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="text-red-500">Vite + React</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <h1 className="text-red-500 text-center text-5xl my-4">Tic Tac Toe</h1>
+      <main>
+        <div className="grid grid-rows-3 grid-cols-3 w-1/2 m-auto gap-3">
+          {[...Array(9).keys()].map((i) => (
+            <Cell key={i} />
+          ))}
+        </div>
+      </main>
     </>
   );
 }
